026575b27963ac023e371f72786546d45eddf95c
joat21 <eridel61@yandex.ru>
2025-10-08 13:36:59 +0500

    feat: в шапку добавлены ссылки для навигации

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <header className="flex justify-center w-full bg-white">
       <div className="container flex justify-between items-center gap-10 ">
-        <span>ServiceDesk</span>
-        <span>Борис Иванов</span>
+        <Link to="/">ServiceDesk</Link>
+        <Link to="/profile">Борис Иванов</Link>
       </div>
     </header>
   );
