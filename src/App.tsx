c779dc1e63ba129079472c54f76bbf6cce764f47
joat21 <eridel61@yandex.ru>
2025-10-08 11:20:23 +0500

    feat: добавлена черновая верстка страницы Профиль

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Card, CardBody, CardHeader, Divider, Image } from '@heroui/react';
+import { Header } from './Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="content pt-24">
+        <div className="container flex justify-between items-center gap-24">
+          <Card className="p-7 max-w-[400px] w-full">
+            <CardHeader className="flex flex-col p-0">
+              <Image
+                alt="heroui logo"
+                height={200}
+                radius="full"
+                src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                width={200}
+                className="mb-14"
+              />
+              <span className="mb-14">Борис Иванов</span>
+            </CardHeader>
+            <Divider />
+            <CardBody className="gap-5 p-0">
+              <div className="flex justify-between">
+                <span>ID сотрудника</span>
+                <span>EMP-0004</span>
+              </div>
+              <div className="flex justify-between">
+                <span>График работы</span>
+                <span>Пн-Пт 9:00-18:00</span>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="p-7 w-full max-h-fit">
+            <CardHeader className="p-0">
+              <span className="text-2xl">Личная информация</span>
+            </CardHeader>
+            <CardBody className="grid grid-cols-2 gap-3 p-0">
+              <div className="flex flex-col gap-1.5">
+                <span className="font-bold">Email</span>
+                <span>ivanov@company.com</span>
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <span className="font-bold">Местоположение</span>
+                <span>Екатеринбург, Генеральская 8</span>
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <span className="font-bold">Телефон</span>
+                <span>+ 7 (495) 123-45-67</span>
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <span className="font-bold">Должность</span>
+                <span>Сотрудник?</span>
+              </div>
+              <div className="flex flex-col gap-1.5">
+                <span className="font-bold">Отдел</span>
+                <span>Маркетинг</span>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+export const Header = () => {
+  return (
+    <header className="flex justify-center w-full bg-white">
+      <div className="container flex justify-between items-center gap-10 ">
+        <span>ServiceDesk</span>
+        <span>Борис Иванов</span>
+      </div>
+    </header>
+  );
+};
