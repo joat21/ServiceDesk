20fd1eebe4b7ce98afe2bff012904a25a9aad3c0
joat21 <eridel61@yandex.ru>
2025-10-08 13:10:56 +0500

    feat: подключена маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import { ProfilePage } from './pages/profile';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route index element={<h1>Дашборд</h1>} />
+      <Route path="profile" element={<ProfilePage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <ProfilePage />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
