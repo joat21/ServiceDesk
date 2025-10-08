21ce3c34e84106862d8765c5fa767a85febc623b
joat21 <eridel61@yandex.ru>
2025-10-08 13:27:50 +0500

    refactor: Header перенесен в layout страниц

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { ProfilePage } from './pages/profile';
+import { MainLayout } from './shared/ui';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/">
+    <Route path="/" element={<MainLayout />}>
       <Route index element={<h1>Дашборд</h1>} />
       <Route path="profile" element={<ProfilePage />} />
     </Route>
