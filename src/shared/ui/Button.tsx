8a05e59781fff1769bc53f3237f82bf7fc6e5f43
joat21 <eridel61@yandex.ru>
2025-11-17 18:16:34 +0500

    style(button): стилизованы состояния компонента

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -4,8 +4,9 @@ import { cn, Button as HeroButton, type ButtonProps } from '@heroui/react';
 const variantClassMap: Partial<
   Record<NonNullable<ButtonProps['variant']>, string>
 > = {
-  solid: 'bg-primary text-primary-foreground hover:bg-primary/90',
-  ghost: 'bg-transparent border border-[#c3c0c0] text-[#666]',
+  solid: 'bg-primary text-primary-foreground data-[hover=true]:bg-[#CA0808]',
+  ghost:
+    'bg-transparent border border-[#c3c0c0] text-[#666] data-[hover=true]:bg-[#ededed]',
 };
 
 export const Button: FC<ButtonProps> = ({
@@ -18,7 +19,7 @@ export const Button: FC<ButtonProps> = ({
     <HeroButton
       variant={variant}
       className={cn(
-        'px-5 py-1 rounded-lg min-h-8 h-8 text-base',
+        'px-5 py-1 rounded-lg min-h-8 h-8 text-base data-[hover=true]:opacity-100 hover:opacity-100',
         variantClassMap[variant],
         className
       )}
