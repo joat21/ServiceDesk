46435372896cff4e7387af284ee5fd3471027a49
joat21 <eridel61@yandex.ru>
2025-12-18 16:36:03 +0500

    fix: добавлено отображение названия приоритета при редактировании коэффициентов срочности

--- src/features/edit-priority-factors/ui/EditPriorityFactorsModal.tsx
+++ src/features/edit-priority-factors/ui/EditPriorityFactorsModal.tsx
@@ -12,10 +12,14 @@ export const EditPriorityFactorsModal: FC<Omit<ModalProps, 'children'>> = ({
   if (!priorities) return <p>Приоритеты не найдены</p>;
 
   return (
-    <Modal title="Редактирование офиса" {...props}>
+    <Modal title="Редактирование коэффициентов" {...props}>
       <ModalBody>
         {priorities.map((priority) => (
-          <NumberInput step={0.01} defaultValue={priority.slaFactor} />
+          <NumberInput
+            label={priority.name}
+            step={0.01}
+            defaultValue={priority.slaFactor}
+          />
         ))}
       </ModalBody>
     </Modal>
